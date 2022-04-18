ba2915e3c38394cdf137e40bc0d49078c4b0f94e
Yorik56 <yorik-ba-56@hotmail.fr>
2022-04-18 05:43:57 +0200

    Clean imports JS

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('nodeModules', './assets/glightbox.js')
+    .addEntry('nodeModules', './assets/nodeModules.js')
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
 
