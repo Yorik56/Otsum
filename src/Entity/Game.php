67cae82d39b89d462abff704c4c58c32711a7d53
Yorik56 <yorik-ba-56@hotmail.fr>
2022-05-18 01:38:22 +0200

    Unused import

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -6,7 +6,6 @@ use App\Repository\GameRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\ORM\PersistentCollection;
 use JetBrains\PhpStorm\Pure;
 use Symfony\Component\Security\Core\User\UserInterface;
 
