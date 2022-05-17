63839391cca4a7bc5d28e704531d9804fbe40394
Yorik56 <yorik-ba-56@hotmail.fr>
2022-05-18 01:37:40 +0200

    Uniq constraint

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 use JetBrains\PhpStorm\Pure;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 
 /**
